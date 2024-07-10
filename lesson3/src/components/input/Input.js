fe32b5a0400ea903bef2e21e1c3d88995e5d71f5
XOMAK12 <nnikitaivanov202@gmail.com>
2024-07-10 19:16:11 +0600

    HW2

---
+++ lesson3/src/components/input/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Input = ({type = 'text', placeholder, onChange}) => {
+    return (
+        <input type={type} onChange={onChange} placeholder={placeholder}/>
+    );
+};
+
+export default Input;
