05517eef8f589e20d4af2e64fc79ccdd43b2e8ff
XOMAK12 <163844436+XOMAK12@users.noreply.github.com>
2024-07-13 01:42:41 +0600

    Delete lesson3 directory

--- lesson3/src/components/input/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Input = ({type = 'text', placeholder, onChange}) => {
-    return (
-        <input type={type} onChange={onChange} placeholder={placeholder}/>
-    );
-};
-
-export default Input;
