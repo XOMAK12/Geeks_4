966e62d48605f44b703c1f24cc75217a2ebed2c0
XOMAK12 <nnikitaivanov202@gmail.com>
2024-07-13 02:15:04 +0600

    HW3

--- lesson2/src/page/mainPage/MainPage.js
+++ lesson2/src/page/mainPage/MainPage.js
@@ -1,59 +1,63 @@
-import React, { useState } from 'react';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
-import User from '../../components/user/User';
-import School from '../../components/school/School';
-import Example from '../../components/example/Example';
-import Button, {Button2} from '../../components/button/Button';
-import Count from '../../components/count/Count';
-import Modal from '../../components/modal/Modal';
-import Input from '../../components/input/Input';
+import React, {useState} from 'react';
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import User from "../../components/user/User";
+import Car from "../../components/car/Car";
+import Schools from "../../components/schools/Schools";
+import Example from "../../components/example/Example";
+import Button from "../../components/button/Button";
+import Count from "../../components/count/Count";
+import Modal from "../../components/modal/Modal";
+import Input from "../../components/input/Input";
 
 
 const MainPage = () => {
     const [show, setShow] = useState(false)
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
+    console.log(show, 'в начале')
     const handleShow = () => {
-        setShow(prev=> !prev)
+        setShow(prev => !prev)
     }
-
-    const handleChange =(event) => {
-        console.log(event.target.value);
-        setInput(event.target.value)
+    const handleChange = (event) => {
+        setInput(event.target.value);
     }
 
     return (
         <div>
-            <Header text={'red'}/>
-            <Header text={'blue'}/>
+            <Header/>
             {
                 show && <Modal handleShow={handleShow}/>
             }
             <h1>{input}</h1>
             <input type="text" onChange={handleChange}/>
-            <Input onChange={handleChange} placeholder={'Введите текст'}/>
-            {/*<Count/>*/}
-            {/*<h1>MainPage</h1>*/}
+            <Input type={'password'} onChange={handleChange} placeholder={'Введите текст'}/>
+            <Count/>
+            <h1>MainPage</h1>
             <div>Show</div>
-            <button onClick={handleShow}>Открыть</button>
-            {
-                show && <div style={{backgroundColor: "red"}}>Модальное окно</div>
-            }
-            <User name={'Bakyt'} age={18} email={'baktybeks@mail.ru'}/>
+
+            <Button name={'Открыть'} onClick={handleShow}/>
+            <div>
+                <h2>Buttons HW</h2>
+                <Button name={'Save'} color={'green'}/>
+                <Button name={'Delete'} color={'red'}/>
+                <Button name={'Edit'} color={'blue'}/>
+            </div>
+            {/*{*/}
+            {/*    show && <div style={{backgroundColor: "red"}}>Модальное окно</div>*/}
+            {/*}*/}
+            {/*<User name={'Nikita'} age={18} email={'nikita@gmail.com'} />*/}
+            {/*<Car car={'Audi'} />*/}
+            {/*<Example title={'Example'}>*/}
+            {/*    <p style={{color : "red"}}>name: Nikita</p>*/}
+            {/*    <p style={{color : "blue"}}>name: Bakyt</p>*/}
+            {/*    <p style={{color : "green"}}>name: Maks</p>*/}
+            {/*    <Schools title={'KSTU'}/>*/}
             {/**/}
-            <Example title={'Example'}>
-                <p style={{color: 'red'}}>name: Bakyt</p>
-                <p style={{color: 'blue'}}>name: Bakyt</p>
-                <p style={{color: 'green'}}>name: Bakyt</p>
-                <p>name: Bakyt</p>
-                <School title={'KSTU'}/>
-
-                <Button name={'Save'} />
-                <Button name={'Edit'} />
-                <Button name={'Delete'} />
-
-            </Example>
-            <Footer/>
+            {/*    <Button name={'Save'}/>*/}
+            {/*    <Button name={'Edit'}/>*/}
+            {/*    <Button name={'Delete'}/>*/}
+            {/*</Example>*/}
+            {/*<Footer/>*/}
         </div>
     );
 };
